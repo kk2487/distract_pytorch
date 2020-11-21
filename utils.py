46f94996e92f8364f5e4ccaa5ed2765b90c94f6f
msi_win <msi_win>
2020-11-21 20:33:39 +0800

    parameters of headpose

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 IMG_SIZE = 256
 LR = 1e-3
+VAR_yaw = 30
+VAR_pitch = -10
+VAR_roll = 0
 
 train_path='/home/hongze/Desktop/dataset/train/'
 validation_path='/home/hongze/Desktop/dataset/validation/'
