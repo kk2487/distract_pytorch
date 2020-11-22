0124f6f1b25835df92962f05cec1ac0ce54a2d15
msi_win <msi_win>
2020-11-22 20:43:37 +0800

    add status fuction

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 IMG_SIZE = 256
 LR = 1e-3
-VAR_yaw = 30
+
+VAR_yaw = 20
 VAR_pitch = -10
 VAR_roll = 0
+
+H_L = -10
+H_R = 10
+H_U = -10
+H_D = 10
+T_L = 10
+T_R = -10
+
 
 train_path='/home/hongze/Desktop/dataset/train/'
 validation_path='/home/hongze/Desktop/dataset/validation/'
